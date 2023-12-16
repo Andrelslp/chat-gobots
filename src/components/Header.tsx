4ae4c4e6e69277cb11c4509524bf96c071d4ca1b
André Souza <98634615+Andrelslp@users.noreply.github.com>
2023-12-16 08:21:06 -0300

    feat:  return to the home page by clicking on the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
+import { useRouter } from 'next/navigation';
 import Logo from './Logo';
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <header className="w-screen bg-dark-3 px-10 py-4">
-      <Logo />
+      <Logo onClick={() => router.push('/')} />
     </header>
   );
 }
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,16 @@
-export default function Logo() {
+interface ILogoProps {
+  onClick?: () => void;
+}
+
+export default function Logo({ onClick }: ILogoProps) {
   return (
     <div>
-      <span className="text-6xl font-bold text-white-1">Chat</span>
-      <span className="text-6xl font-bold text-blue-3">GoBots</span>
+      <span className="text-6xl font-bold text-white-1" onClick={onClick}>
+        Chat
+      </span>
+      <span className="text-6xl font-bold text-blue-3" onClick={onClick}>
+        GoBots
+      </span>
     </div>
   );
 }
