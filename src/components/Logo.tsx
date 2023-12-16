8bec62339bbbade4d11411485a14b18eb1667a1d
André Souza <98634615+Andrelslp@users.noreply.github.com>
2023-12-16 08:32:51 -0300

    feat: add cursor pointer

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,10 +5,16 @@ interface ILogoProps {
 export default function Logo({ onClick }: ILogoProps) {
   return (
     <div>
-      <span className="text-6xl font-bold text-white-1" onClick={onClick}>
+      <span
+        className="cursor-pointer text-6xl font-bold text-white-1"
+        onClick={onClick}
+      >
         Chat
       </span>
-      <span className="text-6xl font-bold text-blue-3" onClick={onClick}>
+      <span
+        className="cursor-pointer text-6xl font-bold text-blue-3"
+        onClick={onClick}
+      >
         GoBots
       </span>
     </div>
